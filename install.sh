75173165654e88538212c585f69e5b54cce359a5
ZhuXiaolong <zhuxiaolong.cd@gmail.com>
2020-10-19 10:46:37 +0800

    something miss

---
+++ install.sh
@@ -0,0 +1,7 @@
+npm install
+npm install hexo-deployer-git --save
+npm install --save hexo-autonofollow hexo-directory-category hexo-generator-feed hexo-generator-json-content hexo-generator-sitemap
+#git clone https://github.com/zthxxx/hexo-theme-Wikitten.git themes/Wikitten
+#cp -rf themes/Wikitten/_source/* source/
+#cp -rf themes/Wikitten/_scaffolds/* scaffolds/
+#cp -f themes/Wikitten/_config.yml.example themes/Wikitten/_config.yml
